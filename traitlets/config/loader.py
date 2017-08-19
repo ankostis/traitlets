7bd0f4b9b594c65cb9075a26484afa9b6ac8495f
Kostis Anagnostopoulos <ankostis@gmail.com>
2017-08-19 18:34:55 +0300

    feat(app): CLI use config-priorities to merge
    + Add `Config.substract()` to extract overriding values only.

--- traitlets/config/loader.py
+++ traitlets/config/loader.py
@@ -209,13 +209,21 @@ class Config(dict):
                 if def_rank != rank:
                     my_pr[key] = rank
 
-    def set_default_rank(self, rank):
-        """set priority-rank(an int) for all values without explicit rank, recursively"""
+    def set_default_rank(self, rank, reset=False):
+        """
+        set priority-rank(an int) for all values without explicit rank, recursively
+
+        :param rest:
+            if true, resets recursively all pre-existent priorities to the given rank
+        """
         my_pr = self._merge_priorities
-        try:
-            my_pr[None] = rank
-        except TypeError:
+        if reset:
             self._merge_priorities = rank
+        else:
+            try:
+                my_pr[None] = rank
+            except TypeError:
+                self._merge_priorities = rank
 
         for v in self.values():
             if isinstance(v, Config):
@@ -253,6 +261,38 @@ class Config(dict):
                 self._set_rank_of(k, other_rank)
 
         self.update(to_update)
+
+    def substract(self, other):
+        """return a new config with all overriding-differences of the `other`"""
+        diffs = type(self)()
+        ## Diffs jusfged against my priorities.
+        diffs._merge_priorities = self.rank_of()
+
+        for k, v in other.items():
+            try:
+                other_rank = other.rank_of(k)
+            except AttributeError:
+                my_rank = other_rank = None
+            else:
+                my_rank = self.rank_of(k)
+
+            if k not in self:
+                # New keys adopted regardless of rank.
+                diffs[k] = v
+            else: # I have this key
+                if isinstance(v, Config) and isinstance(self[k], Config):
+                    # Recursively substract common Configs.
+                    diffs[k] = self[k].substract(v)
+                else: # For non-Configs, diff is anything >= my-rank.
+                    if my_rank is not None and other_rank < my_rank:
+                        continue
+
+                    diffs[k] = v
+
+            if my_rank != other_rank:
+                diffs._set_rank_of(k, other_rank)
+
+        return diffs
 
     def collisions(self, other):
         """Check for collisions between two config objects.
@@ -308,7 +348,7 @@ class Config(dict):
                 # shallow copy plain container traits
                 value = copy.copy(value)
             new_config[key] = value
-        new_config._merge_priorities = copy.deepcopy(self._merge_priorities)
+        new_config._merge_priorities = copy.copy(self._merge_priorities)
         return new_config
 
     def __getitem__(self, key):
